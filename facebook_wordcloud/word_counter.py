1308f5fbae534940cf032a95f5aa2df3e584e9ce
Michael Meli <mjmeli@ncsu.edu>
2016-08-23 12:03:37 -0400

    removed dependency on downloading nltk, instead cache file locally

--- facebook_wordcloud/word_counter.py
+++ facebook_wordcloud/word_counter.py
@@ -2,10 +2,13 @@
     word_counter
     Provides helper functions for word counts.
 """
+import os
 import re
 import nltk
 from collections import Counter
 from nltk.corpus import stopwords
+
+STOPWORDS_FILE = os.path.join(os.path.dirname(__file__), "STOPWORDS")
 
 # Get the frequencies of each word as a dictionary.
 # strings = a list of strings, can be strings of any length/number of words
@@ -32,21 +35,33 @@ def get_frequencies(strings):
 def get_frequencies_tuple(strings):
     return get_frequencies(strings).items()
 
+# Load stopwords from file, if it exists
+def load_stopwords():
+    if os.path.isfile(STOPWORDS_FILE):
+        return set([x.strip() for x in open(STOPWORDS_FILE).read().split('\n')])
+    else:
+        return set()
+
+
 # Filter out stopworks from a frequencies dict or tuple
-def filter_stopwords(frequencies):
+def filter_stopwords(frequencies, additional_stopwords=[]):
     # Convert to tuples for easy of development if dictionary
     is_dict = False
     if type(frequencies) is dict:
         is_dict = True
         frequencies = frequencies.items()
 
-    # Download stopwords
-    nltk.download("stopwords")
+    # Load stopwords from file
+    stopwords = load_stopwords()
+
+    # Add in additional stopwords
+    for word in additional_stopwords:
+        stopwords.add(word)
 
     # Filter out words that are stopwords
     filtered = []
     for word in frequencies:
-        if not word[0] in stopwords.words('english'):
+        if not word[0] in stopwords:
             filtered.append(word)
 
     # Convert back to original form
